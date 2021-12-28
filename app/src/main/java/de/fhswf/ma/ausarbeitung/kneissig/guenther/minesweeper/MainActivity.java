e0c714a1c1c6ec97a3fb8e7746d63829c6043606
Ramon <guenther.ramonantonio@fh-swf.de>
2021-12-28 17:25:03 +0100

    Projektstart

---
+++ app/src/main/java/de/fhswf/ma/ausarbeitung/kneissig/guenther/minesweeper/MainActivity.java
@@ -0,0 +1,15 @@
+package de.fhswf.ma.ausarbeitung.kneissig.guenther.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //R & I - Minesweeper
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
